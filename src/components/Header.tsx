3494ff2775cd56c7e84efe29a68c0ee6828022f5
Rere Tooki <rereloluwa.afolabi.tooki@gmail.com>
2023-08-26 15:02:26 +0100

    Added my intro background gradient

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,20 @@
 import React from "react";
 import "../App.css";
+import bgintromobile from "../assets/images/bg-pattern-intro-mobile.svg";
+import bgintrodesktop from "../assets/images/bg-pattern-intro-desktop.svg";
+import bgrandom from "../assets/images/icon-arrow-dark.svg";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <>
+      <div className="gradient">
+        <picture>
+          <source media="(min-width: 768px)" srcSet={bgintrodesktop} />
+          <img src={bgintromobile} alt="Background icon design" className="" />
+        </picture>
+      </div>
+    </>
+  );
 };
 
 export default Header;
