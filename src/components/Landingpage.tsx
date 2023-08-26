75421a1653b936684e4a9c121a1c241c031477f1
Rere Tooki <rereloluwa.afolabi.tooki@gmail.com>
2023-08-26 13:37:52 +0100

    Created the header & footer components.

--- src/components/Landingpage.tsx
+++ src/components/Landingpage.tsx
@@ -1,8 +1,17 @@
 import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 import "../App.css";
 
 const Landingpage = () => {
-  return <div>Landingpage</div>;
+  return (
+    <>
+      <div className="">
+        <Header />
+        <Footer />
+      </div>
+    </>
+  );
 };
 
 export default Landingpage;
