5ffb06dccf9d692058c767de845b9fe4f8d44224
Rere Tooki <rereloluwa.afolabi.tooki@gmail.com>
2023-08-26 22:57:09 +0100

    Started the bodyintro part

---
+++ src/components/BodyIntro.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../App.css";
+
+const BodyIntro = () => {
+  return (
+    <>
+      <div>
+        <h1>Designed for the future</h1>
+      </div>
+    </>
+  );
+};
+
+export default BodyIntro;
--- src/components/Landingpage.tsx
+++ src/components/Landingpage.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "../components/Header";
+import BodyIntro from "../components/BodyIntro";
 import Footer from "../components/Footer";
 import "../App.css";
 
@@ -8,6 +9,7 @@ const Landingpage = () => {
     <>
       <div className="">
         <Header />
+        <BodyIntro />
         {/* <Footer /> */}
       </div>
     </>
