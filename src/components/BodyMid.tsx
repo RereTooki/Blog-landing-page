f3041518ffb03870d341f5b3f0f32b46feb70eed
Rere Tooki <rereloluwa.afolabi.tooki@gmail.com>
2023-08-28 00:58:11 +0100

    Added the 'BodyMid' component

---
+++ src/components/BodyMid.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../App.css";
+import editormobile from "../assets/images/illustration-editor-mobile.svg";
+import editordesktop from "../assets/images/illustration-editor-desktop.svg";
+
+const BodyMid = () => {
+  return (
+    <>
+      <div className="overflow-hidden font-overpass select-none">
+        <h1 className="text-[40px] ">BodyMid</h1>
+      </div>
+    </>
+  );
+};
+
+export default BodyMid;
--- src/components/Landingpage.tsx
+++ src/components/Landingpage.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../components/Header";
 import BodyIntro from "../components/BodyIntro";
+import BodyMid from "../components/BodyMid";
 import Footer from "../components/Footer";
 import "../App.css";
 
@@ -10,6 +11,7 @@ const Landingpage = () => {
       <div className="">
         <Header />
         <BodyIntro />
+        <BodyMid />
         {/* <Footer /> */}
       </div>
     </>
