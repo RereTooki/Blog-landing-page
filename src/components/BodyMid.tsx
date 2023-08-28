9a3596f0bc0c3c2be9c225ae5d3e6d2c0467bddb
Rere Tooki <rereloluwa.afolabi.tooki@gmail.com>
2023-08-28 19:47:46 +0100

    Done with the desktop layout

--- src/components/BodyMid.tsx
+++ src/components/BodyMid.tsx
@@ -6,19 +6,19 @@ import phones from "../assets/images/illustration-phones.svg";
 const BodyMid = () => {
   return (
     <>
-      <div className="gradient2 font-overpass text-center select-none flex flex-col">
-        <div className="relative h-[250px]">
+      <div className="gradient2 font-overpass text-center select-none flex flex-col nsm:flex-row nsm:text-left">
+        <div className="relative nsm:w-[160%] nsm:h-auto h-[250px]">
           <img
-            className="absolute bottom-[-50px]"
+            className="absolute nsm:bottom-[-60px] nsm:left-[20px] nsm:w-[550px] bottom-[-50px] nsm:h-[370px]"
             src={phones}
             alt="A couple of phones"
           />
         </div>
-        <div className="px-[10%] pt-[4%] pb-[40px]">
-          <h1 className="text-[50px] leading-[60px]  font-semibold text-whites">
+        <div className="px-[10%] pt-[4%] pb-[40px] nsm:pl-[2%] nsm:py-[7%]">
+          <h1 className="text-[50px] nsm:text-[25px] leading-[60px]  font-semibold text-whites nsm:whitespace-nowrap">
             State of the Art Infrastructure
           </h1>
-          <p className="leading-[42px] text-[24px] tracking-wide text-grayish-blues">
+          <p className="leading-[42px] nsm:leading-normal text-[24px] nsm:text-[14px] tracking-wide text-grayish-blues">
             With reliability and speed in mind, worldwide data centers provide
             the backbone for ultra-fast connectivity. This ensures your site
             will load instantly, no matter where your readers are, keeping your
