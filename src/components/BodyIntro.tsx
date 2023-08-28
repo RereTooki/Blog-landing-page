0fe63ae37bc45c4f166feb432a1c794c5786f956
Rere Tooki <rereloluwa.afolabi.tooki@gmail.com>
2023-08-29 00:43:23 +0100

    Adjusted the line height

--- src/components/BodyIntro.tsx
+++ src/components/BodyIntro.tsx
@@ -25,10 +25,10 @@ const BodyIntro = () => {
           </picture>
 
           <div className="nsm:basis-0 nsm:grow nsm:shrink">
-            <h1 className="px-[20px] nsm:px-0 text-[40px] nsm:text-[25px] pt-[8%] pb-[4%] nsm:pb-0 leading-[65px] nsm:whitespace-nowrap text-center nsm:text-left text-very-dark-blues font-semibold drop-shadow-lg">
+            <h1 className="px-[20px] nsm:px-0 text-[40px] nsm:text-[25px] pt-[8%] pb-[4%] nsm:pb-0 leading-[50px] nsm:whitespace-nowrap text-center nsm:text-left text-very-dark-blues font-semibold drop-shadow-lg">
               Introducing an extensible editor
             </h1>
-            <p className="text-center nsm:text-left px-[10%] nsm:px-0 leading-[42px] nsm:leading-normal text-[20px] nsm:text-[16px] tracking-wide nsm:tracking-normal text-very-dark-grayish-blues/80 drop-shadow-[0_20px_20px_rgba(0,0,0,0.15)]">
+            <p className="text-center nsm:text-left px-[10%] nsm:px-0 leading-[35px] nsm:leading-normal text-[20px] nsm:text-[16px] tracking-wide nsm:tracking-normal text-very-dark-grayish-blues/80 drop-shadow-[0_20px_20px_rgba(0,0,0,0.15)]">
               Blogr features an exceedingly intuitive interface which lets you
               focus on one thing: creating content. The editor supports
               management of multiple blogs and allows easy manipulation of
@@ -36,10 +36,10 @@ const BodyIntro = () => {
               plugins and themes provide easy ways to add functionality or
               change the looks of a blog.
             </p>
-            <h1 className="px-[20px] nsm:px-0 text-[40px] nsm:text-[25px] pt-[13%] nsm:pt-[50px] pb-[5%] nsm:pb-0 leading-[65px] nsm:whitespace-nowrap text-center nsm:text-left text-very-dark-blues font-semibold drop-shadow-lg">
+            <h1 className="px-[20px] nsm:px-0 text-[40px] nsm:text-[25px] pt-[13%] nsm:pt-[50px] pb-[5%] nsm:pb-0 leading-[50px] nsm:whitespace-nowrap text-center nsm:text-left text-very-dark-blues font-semibold drop-shadow-lg">
               Robust content management
             </h1>
-            <p className="text-center nsm:text-left px-[10%] nsm:px-0 leading-[42px] nsm:leading-normal text-[20px] nsm:text-[16px] tracking-wide nsm:tracking-normal text-very-dark-grayish-blues/80 drop-shadow-[0_20px_20px_rgba(0,0,0,0.15)]">
+            <p className="text-center nsm:text-left px-[10%] nsm:px-0 leading-[35px] nsm:leading-normal text-[20px] nsm:text-[16px] tracking-wide nsm:tracking-normal text-very-dark-grayish-blues/80 drop-shadow-[0_20px_20px_rgba(0,0,0,0.15)]">
               Flexible content management enables users to easily move through
               posts. Increase the usability of your blog by adding customized
               categories, sections, format, or flow. With this functionality,
